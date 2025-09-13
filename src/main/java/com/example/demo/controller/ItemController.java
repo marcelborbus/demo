1b615257565ba8c5f7332099edce335c10843485
marcelborbus <m@borbus.eu>
2025-09-13 15:07:06 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+
+    @GetMapping("/all")
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("Rickroll");
+    }
+
+
+
+}
